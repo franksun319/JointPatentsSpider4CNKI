7fe18912b2f82e8ca3cb81b39d818b2c09143166
franksun319 <frank.sun.319@gmail.com>
2017-10-09 01:42:33 +0800

    minor changes

--- produce_adjacent_matrix.py
+++ produce_adjacent_matrix.py
@@ -16,8 +16,8 @@ if __name__ == '__main__':
     # year = ['2011', '2012', '2013', '2014', '2015', '2016']
     year = ['2007', '2008', '2009', '2010']
     for y in year:
-        print u'生成' + y + u'年邻接矩阵'
-        my_reader = CsvReader(u'D:/My Works/于东平2016国家自科/PatentSpider_2.7/全国能源电力行业数据/2007-2010全国能源电力联合专利.csv',
+        my_reader = CsvReader(u'D:/My Works/于东平2016国家自科/PatentSpider_2.7/2007-2010全国能源电力联合专利.csv',
                               start_time=y + '-01-01', end_time=y + '-12-31')
+        print u'生成' + y + u'年邻接矩阵'
         my_graph_maker = GraphMaker(my_reader.joint_applicant_list())
         my_graph_maker.write_adjacent_matrix('AM_能源电力_' + y + '.csv')
